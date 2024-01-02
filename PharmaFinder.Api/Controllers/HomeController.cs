e280eb5b806c26df00b12f0e0ef01df065eb4533
saif <saif@LAPTOP-2RM9QTAI>
2024-01-02 09:24:39 +0300

    ss

---
+++ PharmaFinder.Api/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PharmaFinder.Core.Data;
+using PharmaFinder.Core.Service;
+
+namespace PharmaFinder.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IHomeService homeService;
+
+        public HomeController(IHomeService homeService)
+        {
+            this.homeService = homeService;
+        }
+
+        [HttpGet]
+        [Route("GetAllHomes")]
+        public IActionResult GetAllHomes()
+        {
+            try
+            {
+                var homes = homeService.GetAllHomes();
+                return Ok(homes);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while retrieving homes: " + ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("GetHomeById/{id}")]
+        public IActionResult GetHomeById(int id)
+        {
+            try
+            {
+                var home = homeService.GetHomeById(id);
+                if (home == null)
+                    return NotFound();
+
+                return Ok(home);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while retrieving the home: " + ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("CreateHome")]
+        public IActionResult CreateHome(Home homeData)
+        {
+            try
+            {
+                homeService.CreateHome(homeData);
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while creating the home: " + ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("UpdateHome")]
+        public IActionResult UpdateHome(Home homeData)
+        {
+            try
+            {
+                homeService.UpdateHome(homeData);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while updating the home: " + ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("DeleteHome/{id}")]
+        public IActionResult DeleteHome(int id)
+        {
+            try
+            {
+                homeService.DeleteHome(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while deleting the home: " + ex.Message);
+            }
+        }
+    }
+}
