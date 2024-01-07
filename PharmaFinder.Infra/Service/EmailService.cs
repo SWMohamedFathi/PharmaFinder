a081e0c6c21ed3dbafbefe2322b54715faa2ef7b
AHMAD AL ZU'BI <aaalzubi178@cit.just.edu.jo>
2024-01-07 05:08:57 +0300

    update

--- PharmaFinder.Infra/Service/EmailService.cs
+++ PharmaFinder.Infra/Service/EmailService.cs
@@ -37,26 +37,7 @@ namespace PharmaFinder.Infra.Service
             }
         }
 
-        public void SendEmail(SendEmailDto emailDto)
-        {
-            var email = new MimeMessage
-            {
-                Subject = emailDto.Subject,
-                To = { MailboxAddress.Parse(emailDto.To) },
-                Body = new TextPart(TextFormat.Html)
-                {
-                    Text = emailDto.Html
-                },
-                From = { MailboxAddress.Parse(_emailConfig.From) }
-            };
-            using (var smtp = new SmtpClient())
-            {
-                smtp.Connect(_emailConfig.Host, _emailConfig.Port, SecureSocketOptions.StartTls);
-                smtp.Authenticate(_emailConfig.From, _emailConfig.Password);
-                var response = smtp.Send(email);
-                smtp.Disconnect(true);
-            }
-        }
+  
 
     }
 }
